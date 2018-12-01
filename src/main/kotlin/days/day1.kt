4f83a47a9b5eb7c4fc3a347efd7ea90d42ced6bc
Alex Petitjean <alex@tenforwardconsulting.com>
2018-12-01 15:47:30 -0600

    Implement day 1 part 2

--- src/main/kotlin/days/day1.kt
+++ src/main/kotlin/days/day1.kt
@@ -4,14 +4,30 @@ import readInputFile
 
 fun day1() {
     val input = readInputFile("inputs/day1.txt")
-    val answer = solve(input)
-    println(answer)
+    println(solve1(input))
+    println(solve2(input))
 }
 
-fun solve(input: String): Int {
+fun solve1(input: String): Int {
     return parseInput(input).sum()
+}
+
+fun solve2(input: String): Int {
+    var currentFrequency = 0
+    val seenFrequencies = mutableSetOf(currentFrequency)
+
+    parseInput(input).asRepeatedSequence().forEach { change ->
+        currentFrequency += change
+        if (!seenFrequencies.add(currentFrequency)) return currentFrequency
+    }
+    throw Error("how did we get here?")
 }
 
 private fun parseInput(input: String): List<Int> {
     return input.split("\n").map(String::toInt)
 }
+
+private fun <T> List<T>.asRepeatedSequence() =
+    generateSequence(0) {
+        (it + 1) % this.size
+    }.map(::get)
--- src/test/kotlin/day1/Day1Spec.kt
+++ src/test/kotlin/day1/Day1Spec.kt
@@ -1,6 +1,7 @@
 package day1
 
-import days.solve
+import days.solve1
+import days.solve2
 import org.spekframework.spek2.Spek
 import org.spekframework.spek2.style.specification.describe
 import kotlin.test.assertEquals
@@ -8,34 +9,83 @@ import kotlin.test.assertEquals
 object Day1Spec : Spek({
 
     describe("day 1") {
-        val input1 by memoized {
-            """
-            +1
-            +1
-            +1
-            """.trimIndent()
+
+        context("part 1") {
+            val input1 by memoized {
+                """
+                +1
+                +1
+                +1
+                """.trimIndent()
+            }
+
+            val input2 by memoized {
+                """
+                +1
+                +1
+                -2
+                """.trimIndent()
+            }
+
+            val input3 by memoized {
+                """
+                -1
+                -2
+                -3
+                """.trimIndent()
+            }
+
+            it("finds the sum of the numbers") {
+                assertEquals(3, solve1(input1))
+                assertEquals(0, solve1(input2))
+                assertEquals(-6, solve1(input3))
+            }
         }
 
-        val input2 by memoized {
-            """
-            +1
-            +1
-            -2
-            """.trimIndent()
-        }
+        context("part 2") {
+            val input1 by memoized {
+                """
+                    +1
+                    -1
+                """.trimIndent()
+            }
 
-        val input3 by memoized {
-            """
-            -1
-            -2
-            -3
-            """.trimIndent()
-        }
+            val input2 by memoized {
+                """
+                    +3
+                    +3
+                    +4
+                    -2
+                    -4
+                """.trimIndent()
+            }
 
-        it("finds the sum of the numbers") {
-            assertEquals(3, solve(input1))
-            assertEquals(0, solve(input2))
-            assertEquals(-6, solve(input3))
+            val input3 by memoized {
+                """
+                    -6
+                    +3
+                    +8
+                    +5
+                    -6
+                """.trimIndent()
+            }
+
+            val input4 by memoized {
+                """
+                    +7
+                    +7
+                    -2
+                    -7
+                    -4
+                """.trimIndent()
+            }
+
+            it("finds the first frequency that is reached twice") {
+                assertEquals(0, solve2(input1))
+                assertEquals(10, solve2(input2))
+                assertEquals(5, solve2(input3))
+                assertEquals(14, solve2(input4))
+            }
         }
     }
 })
